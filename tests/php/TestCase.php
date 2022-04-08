0fb22a9d84b0c872e40a54e6403803d9c1296f8e
thelovekesh <lovekesh.kumar@rtcamp.com>
2022-04-08 17:20:03 +0530

    Add abstract class that extends WP Testcases

---
+++ tests/php/TestCase.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Provide a base class for all unit tests by extending WP_UnitTestCase.
+ *
+ * @package Elementary
+ */
+
+namespace Elementary\Tests;
+
+use WP_UnitTestCase;
+
+/**
+ * Class TestCase
+ *
+ * @since 1.0.0
+ */
+abstract class TestCase extends WP_UnitTestCase { }
