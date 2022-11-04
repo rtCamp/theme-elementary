e4fd00904188a9eb181c74d998ca5094c2f7af3e
Edi Amin <to.ediamin@gmail.com>
2022-11-04 11:21:16 +0600

    Add a page creation pattern.

---
+++ patterns/page-creation-pattern.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: A page creation pattern.
+ * Slug: elementary-theme/page-creation-pattern
+ * Block Types: core/post-content
+ *
+ * @package Elementary-Theme
+ */
+
+?>
+<!-- wp:paragraph -->
+<p>Page creation patterns are some starter patterns when creating a page. Learn more about <a href="https://make.wordpress.org/core/2022/05/03/page-creation-patterns-in-wordpress-6-0/">Page creation patterns</a>.</p>
+<!-- /wp:paragraph -->
