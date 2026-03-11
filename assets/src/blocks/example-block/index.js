fc881e7e9ec1296beb2cd49c66fdf2696d5eafd3
Sagar Jadhav <sagarjadhav27@gmail.com>
2026-03-11 16:46:44 +0530

    Add: Based Block Structure

--- assets/src/blocks/example-block/index.js
+++ assets/src/blocks/example-block/index.js
@@ -3,6 +3,9 @@ import Edit from './edit';
 import save from './save';
 import metadata from './block.json';
 
+import './editor.scss';
+import './style.scss';
+
 registerBlockType(metadata.name, {
 	edit: Edit,
 	save,
