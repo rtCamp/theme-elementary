6b330912cc4bea000397cd84802f06cda6d33660
Bhavik Tank <bhaviktank1999@gmail.com>
2026-04-23 21:35:35 +0530

    feat: add bootstrap file to phpstan for defining constants

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * PHPStan Bootstrap file.
+ */
+
+define( 'ELEMENTARY_THEME_VERSION', '1.0.0' );
+define( 'ELEMENTARY_THEME_TEMP_DIR', __DIR__ );
+define( 'ELEMENTARY_THEME_BUILD_DIR', __DIR__ . '/assets/build' );
+define( 'ELEMENTARY_THEME_BUILD_URI', 'https://example.com/wp-content/themes/elementary/assets/build' );
