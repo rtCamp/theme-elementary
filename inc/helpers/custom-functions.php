c60aa58a6d78fc377aeebdebadd381ebd2e4e59d
Abhishek Singh <abhisheksinghxix@gmail.com>
2026-04-29 13:16:27 +0530

    Add convenience wrapper for get method

--- inc/helpers/custom-functions.php
+++ inc/helpers/custom-functions.php
@@ -28,3 +28,23 @@ if ( ! function_exists( 'elementary_theme_component' ) ) {
 		ComponentLoader::render( $name, $args, $options );
 	}
 }
+
+if ( ! function_exists( 'elementary_theme_get_component' ) ) {
+
+	/**
+	 * Get the rendered HTML of a component as a string.
+	 *
+	 * Global convenience wrapper for ComponentLoader::get().
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $name    Component name (e.g. 'Button', 'Card').
+	 * @param array  $args    Arguments to pass to the component.
+	 * @param array  $options Optional. Resolution options. See ComponentLoader::get().
+	 *
+	 * @return string Rendered component HTML.
+	 */
+	function elementary_theme_get_component( string $name, array $args = [], array $options = [] ): string {
+		return ComponentLoader::get( $name, $args, $options );
+	}
+}
