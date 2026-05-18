a4feb94c44e5701d893b8370e2828edb2a24f7df
Bhavik Tank <bhaviktank1999@gmail.com>
2026-05-19 03:24:45 +0530

    feat: add assets enqeue by default for the components
    this is added to the component loade and not to the asset because it is alongside the component so needs to be added there

--- inc/Framework/ComponentLoader.php
+++ inc/Framework/ComponentLoader.php
@@ -38,9 +38,9 @@ class ComponentLoader {
 	 */
 	public static function render( string $name, array $args = [], array $options = [] ): void {
 
-		$file = self::get_component_file( $name, $options );
-		
-		if ( false === $file ) {
+		$component = self::get_component_data( $name, $options );
+
+		if ( false === $component ) {
 			return;
 		}
 
@@ -70,12 +70,15 @@ class ComponentLoader {
 			$enqueue
 		);
 
-		$options['script'] = $enqueue['script'];
-		$options['style']  = $enqueue['style'];
+		$options['script']    = $enqueue['script'];
+		$options['style']     = $enqueue['style'];
+		$options['component'] = $component;
 
 		do_action( 'elementary_theme_before_get_component', $name, $args, $options );
 
-		require $file;
+		require $component['file'];
+
+		self::enqueue_component_assets( $component, $options );
 
 		do_action( 'elementary_theme_after_get_component', $name, $args, $options );
 	}
@@ -113,9 +116,9 @@ class ComponentLoader {
 	 * @param string $name    Component name.
 	 * @param array  $options Resolution options.
 	 *
-	 * @return string|false Full file path on success, false if not found.
+	 * @return array<string, mixed>|false Component metadata on success, false if not found.
 	 */
-	private static function get_component_file( string $name, array $options = [] ): string|false {
+	private static function get_component_data( string $name, array $options = [] ): array|false {
 
 		$component_name = self::normalize_component_name( $name );
 
@@ -129,24 +132,33 @@ class ComponentLoader {
 		 * Filters the registered component paths.
 		 *
 		 * Each entry is keyed by source type ('theme', 'plugin') and maps
-		 * to a directory path where components are stored.
+		 * to either a component PHP directory path or a path config with
+		 * PHP, style, and script locations.
 		 *
 		 * @since 1.0.0
 		 *
-		 * @param array  $paths    Associative array of source => directory path.
+		 * @param array  $paths    Associative array of source => path config.
 		 * @param string $name     Component name being resolved.
 		 * @param array  $options  Options passed to render().
 		 */
 		$paths = apply_filters(
 			'elementary_theme_component_paths',
 			[
-				'theme' => ELEMENTARY_THEME_TEMP_DIR . '/src/Components',
+				'theme' => [
+					'php'    => ELEMENTARY_THEME_TEMP_DIR . '/src/Components',
+					'style'  => [
+						'dir' => ELEMENTARY_THEME_BUILD_DIR . '/css/components',
+						'url' => ELEMENTARY_THEME_BUILD_URI . '/css/components',
+					],
+					'script' => [
+						'dir' => ELEMENTARY_THEME_BUILD_DIR . '/js/components',
+						'url' => ELEMENTARY_THEME_BUILD_URI . '/js/components',
+					],
+				],
 			],
 			$component_name,
 			$options
 		);
-
-		$paths = self::sanitize_component_paths( $paths );
 
 		if ( empty( $paths ) ) {
 			return false;
@@ -157,14 +169,21 @@ class ComponentLoader {
 
 		foreach ( $order as $source ) {
 
-			if ( empty( $paths[ $source ] ) ) {
+			if ( empty( $paths[ $source ]['php'] ) ) {
 				continue;
 			}
 
-			$file = trailingslashit( $paths[ $source ] ) . $component_name . '/' . $component_name . '.php';
+			$file = trailingslashit( $paths[ $source ]['php'] ) . $component_name . '/' . $component_name . '.php';
 
 			if ( file_exists( $file ) && is_readable( $file ) ) {
-				return $file;
+				return [
+					'name'   => $component_name,
+					'source' => $source,
+					'file'   => $file,
+					'root'   => $paths[ $source ]['php'],
+					'paths'  => $paths[ $source ],
+					'assets' => self::get_component_assets( $component_name, $paths[ $source ] ),
+				];
 			}
 		}
 
@@ -172,35 +191,157 @@ class ComponentLoader {
 	}
 
 	/**
-	 * Sanitize component path mappings returned by filters.
+	 * Get component asset metadata.
 	 *
-	 * @param mixed $paths Potentially filtered paths value.
+	 * @param string               $component_name Component name.
+	 * @param array<string, mixed> $paths          Component path config.
 	 *
-	 * @return array<string, string> Valid source => path mappings.
+	 * @return array<string, array<string, string>> Asset metadata.
 	 */
-	private static function sanitize_component_paths( mixed $paths ): array {
-		if ( ! is_array( $paths ) ) {
-			return [];
-		}
+	private static function get_component_assets( string $component_name, array $paths ): array {
+		$assets = [];
 
-		$sanitized_paths = [];
-
-		foreach ( $paths as $source => $path ) {
-			if ( ! is_string( $source ) || ! is_string( $path ) ) {
+		foreach (
+			[
+				'style'  => 'css',
+				'script' => 'js',
+			] as $asset_type => $extension
+		) {
+			if ( empty( $paths[ $asset_type ]['dir'] ) || empty( $paths[ $asset_type ]['url'] ) ) {
 				continue;
 			}
 
-			$source = trim( $source );
-			$path   = trim( $path );
-
-			if ( '' === $source || '' === $path ) {
-				continue;
-			}
-
-			$sanitized_paths[ $source ] = $path;
+			$assets[ $asset_type ] = [
+				'file' => trailingslashit( $paths[ $asset_type ]['dir'] ) . strtolower( $component_name ) . '.' . $extension,
+				'url'  => trailingslashit( $paths[ $asset_type ]['url'] ) . strtolower( $component_name ) . '.' . $extension,
+			];
 		}
 
-		return $sanitized_paths;
+		return $assets;
+	}
+
+	/**
+	 * Enqueue assets for a rendered component.
+	 *
+	 * @param array<string, mixed> $component Component metadata.
+	 * @param array<string, mixed> $options   Component render options.
+	 *
+	 * @return void
+	 */
+	private static function enqueue_component_assets( array $component, array $options ): void {
+		if ( empty( $component['name'] ) || empty( $component['assets'] ) || ! is_array( $component['assets'] ) ) {
+			return;
+		}
+
+		$slug = sanitize_key( (string) $component['name'] );
+
+		if ( ! empty( $options['style'] ) ) {
+			$handle = 'elementary-theme-component-' . $slug . '-style';
+
+			if ( self::register_component_style( $handle, $component['assets']['style'] ) ) {
+				wp_enqueue_style( $handle );
+			}
+		}
+
+		if ( ! empty( $options['script'] ) ) {
+			$handle = 'elementary-theme-component-' . $slug . '-script';
+
+			if ( self::register_component_script( $handle, $component['assets']['script'] ) ) {
+				wp_enqueue_script( $handle );
+			}
+		}
+	}
+
+	/**
+	 * Register a component script.
+	 *
+	 * @param string           $handle    Name of the script. Should be unique.
+	 * @param array            $asset     Component asset metadata.
+	 * @param array<string>    $deps      Optional. An array of registered script handles this script depends on. Default empty array.
+	 * @param string|bool|null $ver       Optional. String specifying script version number, if not set, filetime will be used as version number.
+	 * @param bool             $in_footer Optional. Whether to enqueue the script before </body> instead of in the <head>.
+	 *
+	 * @return bool Whether the script has been registered.
+	 */
+	private static function register_component_script( string $handle, array $asset, array $deps = [], string|bool|null $ver = false, bool $in_footer = true ): bool {
+		if (
+			empty( $asset['url'] ) ||
+			empty( $asset['file'] ) ||
+			! file_exists( $asset['file'] )
+		) {
+			return false;
+		}
+
+		$asset_meta = self::get_component_asset_meta( (string) $asset['file'], $deps, $ver );
+
+		return wp_register_script( $handle, (string) $asset['url'], $asset_meta['dependencies'], $asset_meta['version'], $in_footer );
+	}
+
+	/**
+	 * Register a component stylesheet.
+	 *
+	 * @param string           $handle Name of the stylesheet. Should be unique.
+	 * @param array            $asset  Component asset metadata.
+	 * @param array<string>    $deps   Optional. An array of registered stylesheet handles this stylesheet depends on. Default empty array.
+	 * @param string|bool|null $ver    Optional. String specifying style version number, if not set, filetime will be used as version number.
+	 * @param string           $media  Optional. The media for which this stylesheet has been defined.
+	 *
+	 * @return bool Whether the style has been registered.
+	 */
+	private static function register_component_style( string $handle, array $asset, array $deps = [], string|bool|null $ver = false, string $media = 'all' ): bool {
+		if (
+			empty( $asset['url'] ) ||
+			empty( $asset['file'] ) ||
+			! file_exists( $asset['file'] )
+		) {
+			return false;
+		}
+
+		$asset_meta = self::get_component_asset_meta( (string) $asset['file'], $deps, $ver );
+
+		return wp_register_style( $handle, (string) $asset['url'], $asset_meta['dependencies'], $asset_meta['version'], $media );
+	}
+
+	/**
+	 * Get component asset dependencies and version info from a matching .asset.php file.
+	 *
+	 * @param string           $file Asset file path.
+	 * @param array<string>    $deps Asset dependencies to merge with.
+	 * @param string|bool|null $ver  Asset version string.
+	 *
+	 * @return array<string, mixed> Asset meta information including dependencies and version.
+	 */
+	private static function get_component_asset_meta( string $file, array $deps = [], string|bool|null $ver = false ): array {
+		$normalized_file   = ltrim( str_replace( '\\', '/', $file ), '/' );
+		$asset_meta_target = preg_replace( '/\.[^\/.]+$/', '', $normalized_file );
+		$asset_meta_target = ! empty( $asset_meta_target ) ? $asset_meta_target : $normalized_file;
+		$asset_meta_file   = '/' . $asset_meta_target . '.asset.php';
+		$asset_meta        = is_readable( $asset_meta_file )
+			? require $asset_meta_file
+			: [
+				'dependencies' => [],
+				'version'      => self::get_component_file_version( $file, $ver ),
+			];
+
+		$asset_meta['dependencies'] = array_merge( $deps, $asset_meta['dependencies'] );
+
+		return $asset_meta;
+	}
+
+	/**
+	 * Get component asset file version.
+	 *
+	 * @param string           $file File path.
+	 * @param string|bool|null $ver  File version.
+	 *
+	 * @return int|string|bool File version based on file modification time or provided version.
+	 */
+	private static function get_component_file_version( string $file, string|bool|null $ver = false ): int|string|bool {
+		if ( ! empty( $ver ) ) {
+			return $ver;
+		}
+
+		return file_exists( $file ) ? (string) filemtime( $file ) : false;
 	}
 
 	/**
