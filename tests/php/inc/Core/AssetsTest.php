342416ad56ba4a70752612d3a4ac88929671b28d
Swanand01 <swanandmathekar@gmail.com>
2026-05-29 20:38:39 +0530

    test: egister_style version fix

--- tests/php/inc/Core/AssetsTest.php
+++ tests/php/inc/Core/AssetsTest.php
@@ -108,7 +108,7 @@ class AssetsTest extends TestCase {
 		add_filter( 'elementary_theme_tailwind_enabled', '__return_true' );
 		$instance = new Assets();
 
-		wp_register_style( 'elementary-theme-tailwind', 'https://example.com/tailwind.css' );
+		wp_register_style( 'elementary-theme-tailwind', 'https://example.com/tailwind.css', [], false );
 		$instance->enqueue_assets();
 
 		remove_filter( 'elementary_theme_tailwind_enabled', '__return_true' );
@@ -123,7 +123,7 @@ class AssetsTest extends TestCase {
 		add_filter( 'elementary_theme_tailwind_enabled', '__return_false' );
 		$instance = new Assets();
 
-		wp_register_style( 'elementary-theme-tailwind', 'https://example.com/tailwind.css' );
+		wp_register_style( 'elementary-theme-tailwind', 'https://example.com/tailwind.css', [], false );
 		$instance->enqueue_assets();
 
 		remove_filter( 'elementary_theme_tailwind_enabled', '__return_false' );
